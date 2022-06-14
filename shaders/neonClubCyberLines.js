121455730403586faf6eae450ce51f5728ba6cbb
GitWillG <36644824+GitWillG@users.noreply.github.com>
2022-06-14 19:50:11 -0400

    matching shader to upstream

--- shaders/neonClubCyberLines.js
+++ shaders/neonClubCyberLines.js
@@ -34,7 +34,7 @@ const neonClubCyberLinesFragment = `
   uniform float uBeat1;
   uniform float uBeat2;
 
-  #define tau 6.2831853
+  #define tau PI * 2.0
 
   float circ(vec2 p) {
     float r = length(p);
@@ -44,7 +44,7 @@ const neonClubCyberLinesFragment = `
   }
   
   void main() {
-    float time = uTime/500.;
+    float time = uTime/450.;
     vec2 uv = vUv;
     // uv *= 2.;
     vec4 texture1 = vec4(texture2D(uTexture, uv));
@@ -55,30 +55,14 @@ const neonClubCyberLinesFragment = `
     float rz = vUv.x + 1.;
   
     //rings
-    p /= exp(mod(time, 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679));
-    // p /= exp(mod(1.2 , 3.14159 ));
-    rz /= pow(abs((0. - circ(p))), .5);
+    p /= exp(mod(time, PI));
+    rz /= pow(abs((0.6 - circ(p))), .5);
+    rz *= 2.0;
     vec3 moodColor = uMood;
-    vec3 col = moodColor * rz * 2.;
+    vec3 col = moodColor * rz;
+    vec3 revCol = (1. - texture1.a) * (1. - rz) * col * sin(time + clamp(uBeat2,0.0,5.0));
     col *= texture1.a;
-    // if(uBeat1 <= 0.33 && uBeat1 > 0.){
-    //   col += texture2.r;
-    // }
-    // if(uBeat1 <= 0.66 && uBeat1 > 0.33){
-    //   col += texture2.g;
-    // }
-    // if(uBeat1 <= 1. && uBeat1 > 0.66){
-    //   col += texture2.b;
-    // }
-    // if(uBeat2 <= 0.33 && uBeat2 > 0.){
-    //   col += texture3.r;
-    // }
-    // if(uBeat2 <= 0.66 && uBeat2 > 0.33){
-    //   col += texture3.g;
-    // }
-    // if(uBeat2 <= 1. && uBeat2 > 0.66){
-    //   col += texture3.b;
-    // }
+    col += revCol;
     gl_FragColor = vec4(col , 1.) ;
   ${THREE.ShaderChunk.logdepthbuf_fragment}
   }
